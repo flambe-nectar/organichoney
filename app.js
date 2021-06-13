c93f93eff8885a3993b0dc15b0a55ff3077ee964
flambe-nectar <78646547+flambe-nectar@users.noreply.github.com>
2021-06-13 16:23:54 +0100

    add initial files

---
+++ app.js
@@ -0,0 +1,26 @@
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade  ease forwards ${index / 7 + 0.1}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+
+}
+
+navSlide();
+
+// 0.5s
